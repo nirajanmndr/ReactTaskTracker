2194ac417b8b1eff4992f2428d92b5f78dfea962
nirajanmndr <nirajan.rdm.github@gmail.com>
2021-08-19 17:23:37 +1000

    Getting hands dirty with React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import {useState} from 'react'
 
-function App() {
+const App = () => {
+  const[tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Application 190',
+        day: 'Nov 13',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'VISA',
+        day: 'September 11',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Vechile Registration',
+        day: 'ASAP',
+        reminder: true,
+    },
+])
+
+//Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header />
+      {
+//        <Tasks tasks={tasks} onDelete = {deleteTask}/>
+        tasks.length > 0 ? (<Tasks tasks={tasks} onDelete = {deleteTask}/>) : ('No Task to Show')
+      }  
     </div>
   );
 }
